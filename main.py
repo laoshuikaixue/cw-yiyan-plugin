66edad3630d8af808ad152d2d3d5533181701a9d
RinLit <lintusup@foxmail.com>
2024-12-30 15:10:27 +0800

    fix: 修复在初次添加插件时因没有添加小组件导致的崩溃 (#1)
    * fix: 修复在未添加小组件时启动崩溃的问题
    
    ---------
    
    Co-authored-by: YourUsername <YourEmail>

--- main.py
+++ main.py
@@ -148,6 +148,8 @@ class Plugin:
 
     def auto_scroll(self):
         """自动滚动功能"""
+        if self.test_widget is None:  # 如果小组件不存在，则不执行
+            return
         scroll_area = self.test_widget.findChild(QScrollArea)
         if scroll_area:
             vertical_scrollbar = scroll_area.verticalScrollBar()
